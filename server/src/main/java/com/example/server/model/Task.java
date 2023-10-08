6986517c4ab3b2623c04343b9a5ed681bd2af86e
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-10-08 22:50:33 +0200

    created models

---
+++ server/src/main/java/com/example/server/model/Task.java
@@ -0,0 +1,57 @@
+package com.example.server.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="TASK_ENTITY")
+public class Task {
+    @Id
+    @Column(name="ID")
+    @UuidGenerator
+    private String id;
+
+
+    @Size(max=30,message="Title must be max 30 characters")
+    @NotNull
+    @Column(name="title")
+    private String title;
+
+    @Size(max=250, message="Description must be max 250 characters")
+    @Column(name="description")
+    private String description;
+
+    @OneToMany
+    private List<User> users;
+
+    //comments
+
+    //enum status
+    @Enumerated(EnumType.STRING)
+    @Column(name="status")
+    private Status status;
+
+    @Column(name="created_at")
+    @CreationTimestamp(source = SourceType.DB)
+    private Instant createdAt;
+
+    @Column(name="updated_at")
+    @UpdateTimestamp(source = SourceType.DB)
+    private Instant updatedAt;
+}
