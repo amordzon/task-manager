16a884c80e9c2f04d6b3b1119719d66e1b9690cf
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-11-01 20:49:27 +0100

    added react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppRoutes from "./AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </React.StrictMode>
+);
