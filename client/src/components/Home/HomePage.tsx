f96ae5abfb75a78e3ea6345405ee3d243d6f09b8
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-11-02 21:51:05 +0100

    import jumbotron from different file

--- client/src/components/Home/HomePage.tsx
+++ client/src/components/Home/HomePage.tsx
@@ -1,32 +1,13 @@
 import React from "react";
 import NavbarPage from "./NavbarPage";
 import "../../styles/HomePage.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import Jumbotron from "./Jumbotron";
+
 const HomePage = () => {
   return (
     <div>
       <NavbarPage />
-      <Container
-        fluid
-        className="d-flex align-items-center justify-content-center __jumbotron"
-      >
-        <Row>
-          <Col className="text-center">
-            <h1 className="display-4">Hello, world!</h1>
-            <p className="lead">
-              This is a simple hero unit, a simple jumbotron-style component for
-              calling extra attention to featured content or information.
-            </p>
-            <p>
-              It uses utility classes for typography and spacing to space
-              content out within the larger container.
-            </p>
-            <Button variant="light" size="lg" href="#">
-              Learn more
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+      <Jumbotron />
     </div>
   );
 };
