4ee1ef3e5d2db25441ee320c3e2ef480d3965024
amordzon <97406794+amordzon@users.noreply.github.com>
2023-12-18 23:26:02 +0100

    feat: projects sidebar (#21)
    * added groups to sidebar
    
    * fixed scrollbar
    
    * set active link sidebar
    
    * added icons

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -4,12 +4,30 @@ import useLogout from "../hooks/useLogout";
 import { Navigate } from "react-router";
 import useModal from "../hooks/useModal";
 import NewProject from "./Dashboard/Projects/NewProject";
+import { useSelector } from "react-redux";
+import { Group } from "../types/Group";
+import { RootState } from "../store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faHouse,
+  faLayerGroup,
+  faPlus,
+  faRightFromBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   const [activeLink, setActiveLink] = useState("Home");
   const { isLoggedIn, logOut } = useLogout();
   const { handleShowProjectForm, showNewProjectForm, handleCloseProjectForm } =
     useModal();
+  const [openCollapse, setOpenCollapse] = useState(false);
+  const groups = useSelector((state: RootState) => state.myGroups);
+
+  const handleCollapseToggle = () => {
+    setOpenCollapse(!openCollapse);
+  };
 
   if (!isLoggedIn) {
     return <Navigate to="/" />;
@@ -36,7 +54,8 @@ const Sidebar = () => {
               onClick={() => setActiveLink("Home")}
               active={activeLink === "Home"}
             >
-              Home
+              <FontAwesomeIcon icon={faHouse} />
+              <span className="ms-2">Home</span>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
@@ -45,29 +64,46 @@ const Sidebar = () => {
               onClick={() => setActiveLink("Activity")}
               active={activeLink === "Activity"}
             >
-              Activity
+              <FontAwesomeIcon icon={faBell} />
+              <span className="ms-2">Activity</span>
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              href="#"
-              onClick={() => setActiveLink("Projects")}
-              active={activeLink === "Projects"}
-            >
-              Projects
-            </Nav.Link>
-          </Nav.Item>
+          <Nav.Link onClick={handleCollapseToggle}>
+            <FontAwesomeIcon icon={faLayerGroup} />
+            <span className="ms-2">Projects</span>
+          </Nav.Link>
+          <div className={`__submenu ${openCollapse ? "show" : ""}`}>
+            {groups.myGroups.length > 0 &&
+              groups.myGroups.map((group: Group, index: number) => (
+                <Nav.Item key={index}>
+                  <Nav.Link
+                    href="#"
+                    onClick={() => setActiveLink(group.name)}
+                    active={activeLink === group.name}
+                  >
+                    {group.name}
+                  </Nav.Link>
+                </Nav.Item>
+              ))}
+          </div>
           <Nav.Item>
             <Nav.Link href="#" onClick={handleShowProjectForm} active={false}>
-              Add New
+              <FontAwesomeIcon icon={faPlus} />
+              <span className="ms-2">Add New</span>
             </Nav.Link>
           </Nav.Item>
         </Nav>
 
         <NavDropdown title="User" id="dropdown" className="dropdown mt-auto">
-          <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+          <NavDropdown.Item href="#">
+            <FontAwesomeIcon icon={faUser} />
+            <span className="ms-2">Profile</span>
+          </NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item onClick={logOut}>Sign out</NavDropdown.Item>
+          <NavDropdown.Item onClick={logOut}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span className="ms-2">Sign out</span>
+          </NavDropdown.Item>
         </NavDropdown>
       </Col>
     </>
